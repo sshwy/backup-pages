0bd39e77d87bd8be9f320e045eff3cde43e5ebb1
sshwy <jy.cat@qq.com>
2021-07-17 07:29:47 +0000

    Site updated: 2021-07-17 07:29:46

---
+++ static/beibishi2021/precache-manifest.ce2cbc070237737ce28735146afaa29b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "472050a8a538d6616f55",
+    "url": "/static/beibishi2021/css/app.0e3970a8.css"
+  },
+  {
+    "revision": "2df85c0709d6d202984ce774066cd9a4",
+    "url": "/static/beibishi2021/index.html"
+  },
+  {
+    "revision": "472050a8a538d6616f55",
+    "url": "/static/beibishi2021/js/app.8feb155f.js"
+  },
+  {
+    "revision": "54eccf19427e32d855f9",
+    "url": "/static/beibishi2021/js/chunk-vendors.001dc48a.js"
+  },
+  {
+    "revision": "f03e298bb13fcd7d62127b5a028d7f42",
+    "url": "/static/beibishi2021/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/static/beibishi2021/robots.txt"
+  }
+]);
